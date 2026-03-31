e88940ef015841783a9cbed1552b461c96aff9b9
Linh T Nguyen <linh.nguyen6@wsu.edu>
2026-03-30 21:01:25 -0700

    added cors_origins

--- Backend/fastapi_backend/app/config.py
+++ Backend/fastapi_backend/app/config.py
@@ -14,6 +14,8 @@ class Settings(BaseSettings):
     access_token_expire_minutes: int = 60 * 8
 
     cors_origins: list[str] = [
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
     ]
